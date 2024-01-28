71b21f3583b2f9e02cde6aa1598747dbc71b690e
Woojin Park <147019612+woojinp1994@users.noreply.github.com>
2024-01-27 22:24:24 -0800

    another test

---
+++ config/fonts/fonts.constants.js
@@ -0,0 +1,21 @@
+const FontVariant = {
+    REGULAR: 'regular',
+    MEDIUM: 'medium',
+    BOLD: 'bold',
+  };
+  
+  const FontStyle = {
+    NORMAL: 'normal',
+    ITALIC: 'italic',
+  };
+  
+  const FontDisplay = {
+    SWAP: 'swap',
+  };
+  
+  module.exports = {
+    FontVariant,
+    FontStyle,
+    FontDisplay,
+  };
+  
